1a61f3fa64a8134687a8f659b2af07903a4164ff
ghsanece <ghsanece@gmail.com>
2025-11-03 12:15:20 +0530

    Add files via upload

--- TaskExecutorServiceTest.java
+++ TaskExecutorServiceTest.java
@@ -18,45 +18,58 @@ public class TaskExecutorServiceTest {
 
 		Main.Task<String> taskA1 = new Main.Task<>(UUID.randomUUID(), groupA, Main.TaskType.READ, "taskA1", () -> {
 			log.info("taskA1 running");
+			Thread.sleep(5000);
 			return "A1 completed";
 		});
 
 		Main.Task<String> taskA2 = new Main.Task<String>(UUID.randomUUID(), groupA, Main.TaskType.WRITE, "taskA2",() -> {
 			log.info("taskA2 running");
+			Thread.sleep(7000);
 			return "A2 completed";
 		});
 
 		Main.Task<String> taskA3 = new Main.Task<String>(UUID.randomUUID(), groupA, Main.TaskType.WRITE, "taskA3",() -> {
 			log.info("taskA3 running");
+			Thread.sleep(3000);
 			return "A3 completed";
 		});
 
 		Main.Task<String> taskB1 = new Main.Task<String>(UUID.randomUUID(), groupB, Main.TaskType.WRITE, "taskB1",() -> {
 			log.info("taskB1 running");
+			Thread.sleep(5000);
 			return "B1 completed";
 		});
 		
 		Main.Task<String> taskB2 = new Main.Task<String>(UUID.randomUUID(), groupB, Main.TaskType.READ, "taskB2",() -> {
 			log.info("taskB2 running");
+			Thread.sleep(3000);
 			return "B2 completed";
 		});
 		
 		Main.Task<String> taskC1 = new Main.Task<String>(UUID.randomUUID(), groupC, Main.TaskType.WRITE, "taskC1",() -> {
 			log.info("taskC1 running");
+			Thread.sleep(2000);
 			return "C1 completed";
 		});
 		
 		Main.Task<String> taskC2 = new Main.Task<String>(UUID.randomUUID(), groupC, Main.TaskType.READ, "taskC2",() -> {
 			log.info("taskC2 running");
+			Thread.sleep(1000);
 			return "C2 completed";
 		});
 
 		Future<String> futureA1 = executor.submitTask(taskA1);
+		Thread.sleep(2);
 		Future<String> futureA2 = executor.submitTask(taskA2);
+		Thread.sleep(2);
 		Future<String> futureA3 = executor.submitTask(taskA3);
+		Thread.sleep(2);
 		Future<String> futureB1 = executor.submitTask(taskB1);
+		Thread.sleep(2);
 		Future<String> futureB2 = executor.submitTask(taskB2);
+		Thread.sleep(2);
 		Future<String> futureC1 = executor.submitTask(taskC1);
+		Thread.sleep(2);
 		Future<String> futureC2 = executor.submitTask(taskC2);
 		log.info("Task A1 result: " + futureA1.get());
 		log.info("Task A2 result: " + futureA2.get());
